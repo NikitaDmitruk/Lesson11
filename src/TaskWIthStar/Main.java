24b4032753e4212a8f2a962d84bef0e06b558fe3
NikitaDmitruk <neomouz@icloud.com>
2024-04-28 22:12:34 +0300

    Исправил задачу со *

--- src/TaskWIthStar/Main.java
+++ src/TaskWIthStar/Main.java
@@ -6,29 +6,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите строку: ");
-        if (scanner.hasNext()) {
-            String[] words = scanner.nextLine().split("[,. !?_–]+");
-            for (String a:words) {
-                System.out.println(a);
-            }
-            System.out.println("Для проверки слова на палнидром, введите целое число, равное номеру слова в строке: ");
-            if (scanner.hasNextInt()) {
-                int wordNumber = scanner.nextInt() - 1;
-                if (wordNumber <= words.length) {
-                    StringBuilder newWord = new StringBuilder(words[wordNumber].trim());
-                    if(words[wordNumber].trim().equalsIgnoreCase(String.valueOf(newWord.reverse()))) {
-                        System.out.println("'" + words[wordNumber] + "' палиндром!");
-                    } else {
-                        System.out.println("'" + words[wordNumber] + "' не палиндром!");
-                    }
+        //В 11 строке есть вопрос. Не работает regex [\\W_]+ для русского шрифта. Для латиницы работает.
+        String[] words = scanner.nextLine().split("[,. !?_–:-]+");
+        System.out.println("Для проверки слова на палнидром, введите целое число (начало с 1)" +
+                           ", равное номеру слова в строке: ");
+        if (scanner.hasNextInt()) {
+            int wordNumber = scanner.nextInt() - 1;
+            if (wordNumber < words.length) {
+                StringBuilder newWord = new StringBuilder(words[wordNumber]);
+                if (words[wordNumber].trim().equalsIgnoreCase(String.valueOf(newWord.reverse()))) {
+                    System.out.println("'" + words[wordNumber] + "' палиндром!");
                 } else {
-                    System.out.println("В этой строке меньше слов!");
+                    System.out.println("'" + words[wordNumber] + "' не палиндром!");
                 }
             } else {
-                System.out.println("Вы ввели не целое число!");
+                System.out.println("В этой строке меньше слов!");
             }
         } else {
-            System.out.println("Вы ввели не строку!");
+            System.out.println("Вы ввели не целое число!");
         }
     }
 }
